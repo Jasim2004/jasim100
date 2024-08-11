c5c1176b3ab1959f0f8a7f0f6ca25158f7eb0271
dante9080 <mumthasmv4@gmail.com>
2024-08-11 20:20:19 +0530

    final project

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:flute/login_screen.dart';
+import 'package:partico/Pages/_login_page.dart';
+import 'package:partico/Pages/profile_page.dart'; // Import your ProfilePage
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo List App',
-      home: LoginScreen(),
+      title: 'PARTICO',
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepPurple, // Text color
+          ),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepPurple, // Background color
+          ),
+        ),
+
+        inputDecorationTheme: InputDecorationTheme(
+          floatingLabelBehavior: FloatingLabelBehavior.auto,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+          ),
+        ),
+      ),
+      initialRoute: '/login', // Set initial route to login page
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/profile': (context) => const ProfilePage(), // Add route for profile page
+      },
     );
   }
 }
